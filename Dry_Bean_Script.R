bb7d25938dd30dc0a64a290b0bb2ec100be997da
Marcello Fabrizio <marcfabrizio10@gmail.com>
2021-09-05 17:58:21 -0300

    Perguntas 1 e 2

--- Dry_Bean_Script.R
+++ Dry_Bean_Script.R
@@ -4,6 +4,28 @@ rm(list=ls())
 csv_data = read.csv("Dry_Bean_Dataset.csv",header=T, sep=";", na.strings="?")
 summary(csv_data)
 
-# Remoção das linhas com NA ou NaN
-csv_data = na.omit(csv_data)
+"
+1-
+Carregue a base de dados e mostre a estrutura do dataset (str()). 
+Comente sobre o número de amostras, de variáveis (e seu tipo).
+O arquivo do dataset não pode ser modificado de forma alguma. 
+A leitura deverá ser realizada de tal maneira qualquer característica dos dados. 
+"
 str(csv_data)
+# 13611 observações com 17 variáveis cada
+
+csv_data$Class = factor(csv_data$Class)
+
+"
+2-
+Altere a variável do tipo do feijão (Class) para um factor. 
+Utilizando um comando mostre como estimar o número de classes existentes. 
+"
+summary(csv_data$Class)
+length(unique(csv_data$Class))
+#7 classes de feijões
+
+"
+3-
+Quantas amostras existem por classe? Use um gráfico de barras para ilustrar as quantidades.
+"
